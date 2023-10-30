7854b1c6ef458a45649f0f199028ad1f660b50d6
jasonballejo <ballejo19@gmail.com>
2023-10-30 10:53:31 -0400

    python commit

---
+++ test.py
@@ -0,0 +1,15 @@
+msg = "Hello World"
+
+# Variables
+x = 10
+y = x + 10
+# print("Variable", y)
+
+# Boolan
+boy = True
+girl = False
+
+if boy == True:
+    print("I am a boy") 
+else: 
+    print("I am a Girl")
